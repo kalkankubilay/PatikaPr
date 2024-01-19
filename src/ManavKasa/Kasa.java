06cc97067255c69ea3dd4a5597ba1f8808cf2652
kalkankubilay <kubilaykalkan275@hotmail.com>
2024-01-19 23:46:49 +0300

    Manav Kasa Programı

---
+++ src/ManavKasa/Kasa.java
@@ -0,0 +1,42 @@
+package ManavKasa;
+
+import java.util.Scanner;
+
+public class Kasa {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Manava hoşgeldiniz fiyatlar şöyledir(kg); ");
+        System.out.println("Armut 2,14 tl\n" +
+                "Elma 3,67 tl\n" +
+                "Domates 1,11 tl\n" +
+                "Muz 0,95 tl\n" +
+                "Patlıcan 5,00 tl\n");
+
+        double armut = 2.14;
+        double elma = 3.67;
+        double domates = 1.11;
+        double muz = 0.95;
+        double patlican = 5.00;
+
+        System.out.print("Kaç kilo armut alıcaksınız ? ");
+        int armutKg = scanner.nextInt();
+
+        System.out.print("Kaç kilo elma alıcaksınız ? ");
+        int elmaKg = scanner.nextInt();
+
+        System.out.print("Kaç kilo domates alıcaksınız ? ");
+        int domatesKg = scanner.nextInt();
+
+        System.out.print("Kaç kilo muz alıcaksınız ? ");
+        int muzKg = scanner.nextInt();
+
+        System.out.print("Kaç kilo patlıcan alıcaksınız ? ");
+        int patlicanKg = scanner.nextInt();
+
+        double toplam = ((armut*armutKg)+(elma*elmaKg)+(domates*domatesKg)+(muz*muzKg)+(patlican*patlicanKg));
+        System.out.print("Toplam ödemeniz gereken tutar: " + toplam);
+
+
+    }
+}
