8b19bbbd98460944830238830bf3b3d89209549c
kalkankubilay <kubilaykalkan275@hotmail.com>
2024-01-25 23:32:55 +0300

    Sınıf Geçme Durumu

---
+++ src/SinifGecmeDurumu/sinifgecme.java
@@ -0,0 +1,36 @@
+package SinifGecmeDurumu;
+
+import java.util.Scanner;
+
+public class sinifgecme {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double mat,fizik,kimya,tarih,turkce,muzik;
+
+        System.out.print("Matematik notunuzu giriniz: ");
+        mat = scanner.nextDouble();
+        System.out.print("Fizik notunuzu giriniz: ");
+        fizik = scanner.nextDouble();
+        System.out.print("Kimya notunuzu giriniz: ");
+        kimya = scanner.nextDouble();
+        System.out.print("Tarih notunuzu giriniz: ");
+        tarih = scanner.nextDouble();
+        System.out.print("Turkce notunuzu giriniz: ");
+        turkce = scanner.nextDouble();
+        System.out.print("Müzik notunuzu giriniz: ");
+        muzik = scanner.nextDouble();
+
+        double ort = ((mat+fizik+kimya+tarih+turkce+muzik)/6);
+        System.out.print("Not ortalamanız: " + ort);
+
+        if (ort >= 55){
+            System.out.println(" ");
+            System.out.println("Sınıfı geçtiniz.");
+        }else{
+            System.out.println("Seneye görüşürüz.");
+        }
+        System.out.print("Ortalamanız: " + ort);
+
+    }
+}
