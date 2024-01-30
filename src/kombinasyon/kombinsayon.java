f9e22c21865f34757d166606415a9e39c636aad0
kalkankubilay <kubilaykalkan275@hotmail.com>
2024-01-30 14:44:29 +0300

    Kombinasyon

---
+++ src/kombinasyon/kombinsayon.java
@@ -0,0 +1,42 @@
+package kombinasyon;
+
+import java.util.Scanner;
+
+public class kombinsayon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int totaln = 1;
+        int totalr =1;
+        int totala = 1;
+        int result = 0;
+
+        System.out.print("n değeri giriniz: ");
+        int n = scanner.nextInt();
+
+        System.out.print("r değeri giriniz: ");
+        int r = scanner.nextInt();
+
+        //n! için
+        for (int i=1; i<=n; i++){
+            totaln = totaln*i;
+        }
+
+        //r! için
+        for (int i=1; i<=r; i++){
+            totalr = totalr*i;
+        }
+
+        //n-r! için
+        int a = n-r;
+        for (int i=1; i<=a; i++){
+            totala = totala*i;
+        }
+
+        result = totaln / (totalr * totala);
+        System.out.println("Kombinasyon: " + result);
+
+
+
+
+    }
+}
